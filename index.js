2e1fd3d1656da85bd2c3b401f444ac5376592e02
DilipSha04 <imdilip.004@gmail.com>
2023-05-10 16:45:03 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+ 
+ const openMenu = document.querySelector(".open");
+ const closeMenu = document.querySelector(".close");
+
+ const wrapper = document.querySelector(".sidebar").classList;
+
+ closeMenu.addEventListener("click", ()=>{
+   wrapper.toggle("opened");
+   wrapper.toggle("closed");
+   })
+   
+ openMenu.addEventListener("click", ()=>{
+   wrapper.toggle("opened");
+   wrapper.toggle("closed");
+   })
+ 
